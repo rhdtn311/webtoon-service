7a04c6dd37ee6f62e232e2d8c9d08356d00a8e8c
Kong <rhdtn311@naver.com>
2023-04-07 04:40:40 +0900

    init: 프로젝트 초기 세팅

---
+++ src/main/java/com/kongtoon/WebComicsApplication.java
@@ -0,0 +1,13 @@
+package com.kongtoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebComicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebComicsApplication.class, args);
+	}
+
+}

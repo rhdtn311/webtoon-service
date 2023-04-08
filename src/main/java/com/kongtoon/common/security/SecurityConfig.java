cc3b12468a6ef12d700aaae23ec7e709b55d4bcc
Kong <rhdtn311@naver.com>
2023-04-09 00:18:50 +0900

    add: PasswordEncoder 인터페이스, 구현체 생성
    - PasswordEncoder 는 추후에 다른 암호화 방식을 사용할 수 있을 가능성에 대비하여 인터페이스로 생성
    - 추후 구현체 변경 시 설정 클래스만 변경하도록 수동 빈 등록

---
+++ src/main/java/com/kongtoon/common/security/SecurityConfig.java
@@ -0,0 +1,13 @@
+package com.kongtoon.common.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}

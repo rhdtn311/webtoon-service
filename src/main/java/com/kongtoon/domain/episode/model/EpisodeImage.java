6e76af2535abb40badb318ee7230eb8f670625a4
Kong <rhdtn311@naver.com>
2023-04-24 17:43:13 +0900

    add: episode_image 테이블 추가
    - Episode 의 내용 이미지를 여러 이미지로 분할해서 올릴 수 있기 때문에 테이블을 1:N 으로 분리함

---
+++ src/main/java/com/kongtoon/domain/episode/model/EpisodeImage.java
@@ -0,0 +1,49 @@
+package com.kongtoon.domain.episode.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import com.kongtoon.domain.BaseEntity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "episode_image")
+@Where(clause = "deleted_at IS NULL")
+@SQLDelete(sql = "UPDATE episode_image SET deleted_at = NOW() WHERE id = ?")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class EpisodeImage extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "content_image_url", length = 2048, nullable = false)
+	private String contentImageUrl;
+
+	@Column(name = "content_order", nullable = false)
+	private int contentOrder;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "episode_id", nullable = false)
+	private Episode episode;
+
+	public EpisodeImage(String contentImageUrl, int contentOrder, Episode episode) {
+		this.contentImageUrl = contentImageUrl;
+		this.contentOrder = contentOrder;
+		this.episode = episode;
+	}
+}

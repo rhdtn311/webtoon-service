f13501fc013b86f10a8e7081af5537d2d3325770
Kong <rhdtn311@naver.com>
2023-06-12 17:54:42 +0900

    add: SpringRestDocs + Swagger 문서화 설정 적용
    - openapi3 실행 시 build/api-spec 폴더에 openapi3.yaml 생성
    - copyTest 실행 시 해당 파일이 src/resources/static/docs 에 복사
    - index.html 에서 문서 확인 가능

--- src/main/java/com/kongtoon/common/security/WebConfig.java
+++ src/main/java/com/kongtoon/common/security/WebConfig.java
@@ -1,10 +1,10 @@
 package com.kongtoon.common.security;
 
+import com.kongtoon.common.security.interceptor.AuthenticationInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import com.kongtoon.common.security.interceptor.AuthenticationInterceptor;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -12,5 +12,10 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(new AuthenticationInterceptor());
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
 	}
 }

c89a0b854769c90527126696e0d579ab08d9b347
Kong <rhdtn311@naver.com>
2023-06-17 15:47:06 +0900

    test: 로그인 단위 테스트 코드 작성

--- src/test/java/com/kongtoon/domain/user/service/UserServiceTest.java
+++ src/test/java/com/kongtoon/domain/user/service/UserServiceTest.java
@@ -1,8 +1,13 @@
 package com.kongtoon.domain.user.service;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.mockito.Mockito.*;
-
+import com.kongtoon.common.exception.BusinessException;
+import com.kongtoon.common.exception.ErrorCode;
+import com.kongtoon.common.security.PasswordEncoder;
+import com.kongtoon.domain.user.dto.UserAuthDTO;
+import com.kongtoon.domain.user.dto.request.LoginRequest;
+import com.kongtoon.domain.user.dto.request.SignupRequest;
+import com.kongtoon.domain.user.model.User;
+import com.kongtoon.domain.user.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,12 +15,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import com.kongtoon.common.exception.BusinessException;
-import com.kongtoon.common.exception.ErrorCode;
-import com.kongtoon.common.security.PasswordEncoder;
-import com.kongtoon.domain.user.dto.request.SignupRequest;
-import com.kongtoon.domain.user.model.User;
-import com.kongtoon.domain.user.repository.UserRepository;
+import java.util.Optional;
+
+import static com.kongtoon.utils.TestUtil.createLoginRequest;
+import static com.kongtoon.utils.TestUtil.createUser;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
@@ -111,5 +117,68 @@ class UserServiceTest {
 
 		verify(userRepository).existsByEmail(email);
 		verify(userRepository).existsByLoginId(loginId);
+	}
+
+	@Test
+	@DisplayName("로그인에 성공한다.")
+	void loginSuccess() {
+		// given
+		LoginRequest loginRequest = createLoginRequest();
+		User user = createUser("email@email.com", loginRequest.loginId(), loginRequest.password());
+		UserAuthDTO correctResult = new UserAuthDTO(0L, user.getLoginId(), user.getAuthority());
+
+		when(userRepository.findByLoginId(loginRequest.loginId()))
+				.thenReturn(Optional.of(user));
+		when(passwordEncoder.isMatch(loginRequest.password(), user.getPassword()))
+				.thenReturn(true);
+
+		// when
+		UserAuthDTO result = userService.login(loginRequest);
+
+		// then
+		verify(userRepository).findByLoginId(loginRequest.loginId());
+		verify(passwordEncoder).isMatch(loginRequest.password(), user.getPassword());
+
+		assertThat(result).usingRecursiveComparison()
+				.ignoringFields("userId")
+				.isEqualTo(correctResult);
+	}
+
+	@Test
+	@DisplayName("로그인시 존재하지 않는 로그인 ID로 실패한다.")
+	void loginNotExistLoginIdFail() {
+		// given
+		LoginRequest loginRequest = createLoginRequest();
+
+		when(userRepository.findByLoginId(loginRequest.loginId()))
+				.thenReturn(Optional.empty());
+
+		// when, then
+		assertThatThrownBy(() -> userService.login(loginRequest))
+				.isInstanceOf(BusinessException.class)
+				.hasFieldOrPropertyWithValue("errorCode", ErrorCode.USER_NOT_FOUND);
+
+		verify(userRepository).findByLoginId(loginRequest.loginId());
+	}
+
+	@Test
+	@DisplayName("로그인시 비밀번호 불일치로 실패한다.")
+	void loginPasswordMismatchFail() {
+		// given
+		LoginRequest loginRequest = createLoginRequest();
+		User user = createUser("email@email.com", loginRequest.loginId(), "mismatchPassword");
+
+		when(userRepository.findByLoginId(loginRequest.loginId()))
+				.thenReturn(Optional.of(user));
+		when(passwordEncoder.isMatch(loginRequest.password(), user.getPassword()))
+				.thenReturn(false);
+
+		// when, hen
+		assertThatThrownBy(() -> userService.login(loginRequest))
+				.isInstanceOf(BusinessException.class)
+				.hasFieldOrPropertyWithValue("errorCode", ErrorCode.LOGIN_FAIL);
+
+		verify(userRepository).findByLoginId(loginRequest.loginId());
+		verify(passwordEncoder).isMatch(loginRequest.password(), user.getPassword());
 	}
 }

3ca1be3f1666043d2bfe0a71070e34894ac3ffec
Kong <rhdtn311@naver.com>
2023-06-17 17:17:46 +0900

    test: 로그인 테스트 코드 작성 및 문서화

--- src/test/java/com/kongtoon/domain/user/controller/UserControllerTest.java
+++ src/test/java/com/kongtoon/domain/user/controller/UserControllerTest.java
@@ -4,6 +4,8 @@ import com.epages.restdocs.apispec.ResourceSnippetParameters;
 import com.epages.restdocs.apispec.Schema;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kongtoon.common.exception.ErrorCode;
+import com.kongtoon.common.security.PasswordEncoder;
+import com.kongtoon.domain.user.dto.request.LoginRequest;
 import com.kongtoon.domain.user.dto.request.SignupRequest;
 import com.kongtoon.domain.user.model.User;
 import com.kongtoon.domain.user.repository.UserRepository;
@@ -26,8 +28,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.Optional;
 
 import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.document;
-import static com.kongtoon.utils.TestUtil.createSignupRequest;
-import static com.kongtoon.utils.TestUtil.createUser;
+import static com.kongtoon.utils.TestUtil.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
@@ -44,6 +45,9 @@ class UserControllerTest {
 
 	@Autowired
 	UserRepository userRepository;
+
+	@Autowired
+	PasswordEncoder passwordEncoder;
 
 	@Autowired
 	MockMvc mockMvc;
@@ -250,6 +254,122 @@ class UserControllerTest {
 								fieldWithPath("inputErrors").type(JsonFieldType.ARRAY).description("요청 에러 정보"),
 								fieldWithPath("inputErrors[].message").type(JsonFieldType.STRING).description("요청 에러 메세지"),
 								fieldWithPath("inputErrors[].field").type(JsonFieldType.STRING).description("요청 에러 필드")
+						)
+				)
+		);
+	}
+
+	@Test
+	@DisplayName("로그인에 성공한다.")
+	void loginSuccess() throws Exception {
+		// given
+		LoginRequest loginRequest = createLoginRequest();
+		User user = createUser("email@email.com", loginRequest.loginId(), passwordEncoder.encrypt(loginRequest.password()));
+
+		userRepository.save(user);
+
+		// when
+		ResultActions resultActions = mockMvc.perform(RestDocumentationRequestBuilders.post("/users/login")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(loginRequest)));
+
+		// then
+		resultActions.andExpect(status().isNoContent());
+
+		// docs
+		resultActions.andDo(
+				document("로그인 성공",
+						ResourceSnippetParameters.builder()
+								.tag("로그인")
+								.summary("로그인 성공, 실패 APIs")
+								.requestSchema(Schema.schema("LoginRequest"))
+						,
+						preprocessRequest(prettyPrint()),
+						preprocessResponse(prettyPrint()),
+						requestFields(
+								fieldWithPath("loginId").type(JsonFieldType.STRING).description("로그인ID"),
+								fieldWithPath("password").type(JsonFieldType.STRING).description("비밀번호")
+						)
+				)
+		);
+	}
+
+	@Test
+	@DisplayName("로그인시 존재하지 않는 로그인 ID로 실패한다.")
+	void loginNotExistLoginIdFail() throws Exception {
+		// given
+		LoginRequest loginRequest = createLoginRequest();
+
+		// when
+		ResultActions resultActions = mockMvc.perform(RestDocumentationRequestBuilders.post("/users/login")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(loginRequest)));
+
+		// then
+		resultActions.andExpect(status().isNotFound());
+		resultActions.andExpect(jsonPath("message").value(ErrorCode.USER_NOT_FOUND.getMessage()));
+		resultActions.andExpect(jsonPath("code").value(ErrorCode.USER_NOT_FOUND.name()));
+
+		// docs
+		resultActions.andDo(
+				document("로그인 ID가 존재하지 않아 로그인 실패",
+						ResourceSnippetParameters.builder()
+								.tag("로그인")
+								.summary("로그인 성공, 실패 APIs")
+								.requestSchema(Schema.schema("LoginRequest"))
+						,
+						preprocessRequest(prettyPrint()),
+						preprocessResponse(prettyPrint()),
+						requestFields(
+								fieldWithPath("loginId").type(JsonFieldType.STRING).description("로그인ID"),
+								fieldWithPath("password").type(JsonFieldType.STRING).description("비밀번호")
+						),
+						responseFields(
+								fieldWithPath("message").type(JsonFieldType.STRING).description("실패 메세지"),
+								fieldWithPath("code").type(JsonFieldType.STRING).description("실패 코드"),
+								fieldWithPath("inputErrors").type(JsonFieldType.NULL).description("요청 에러 정보")
+						)
+				)
+		);
+	}
+
+	@Test
+	@DisplayName("로그인시 비밀번호 불일치로 실패한다.")
+	void loginPasswordMismatchFail() throws Exception {
+		// given
+		LoginRequest loginRequest = createLoginRequest();
+		User user = createUser("email@email.com", loginRequest.loginId(), passwordEncoder.encrypt("mismatchPassword"));
+
+		userRepository.save(user);
+
+		// when
+		ResultActions resultActions = mockMvc.perform(RestDocumentationRequestBuilders.post("/users/login")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(loginRequest)));
+
+		// then
+		resultActions.andExpect(status().isConflict());
+		resultActions.andExpect(jsonPath("message").value(ErrorCode.LOGIN_FAIL.getMessage()));
+		resultActions.andExpect(jsonPath("code").value(ErrorCode.LOGIN_FAIL.name()));
+
+		// docs
+		resultActions.andDo(
+				document("비밀번호 불일치로 로그인 실패",
+						ResourceSnippetParameters.builder()
+								.tag("로그인")
+								.summary("로그인 성공, 실패 APIs")
+								.requestSchema(Schema.schema("LoginRequest"))
+						,
+						preprocessRequest(prettyPrint()),
+						preprocessResponse(prettyPrint()),
+						requestFields(
+								fieldWithPath("loginId").type(JsonFieldType.STRING).description("로그인ID"),
+								fieldWithPath("password").type(JsonFieldType.STRING).description("비밀번호")
+						),
+						responseFields(
+								fieldWithPath("message").type(JsonFieldType.STRING).description("실패 메세지"),
+								fieldWithPath("code").type(JsonFieldType.STRING).description("실패 코드"),
+								fieldWithPath("inputErrors").type(JsonFieldType.NULL).description("요청 에러 정보")
 						)
 				)
 		);

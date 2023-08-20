d3298d5808443985988cd3689bcb8e4e48b62ec4
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2023-08-20 14:15:55 +0900

    chore: 이메일 검증 시 정확한 필드 명이 응답되도록 Validator 수정

--- src/main/java/com/kongtoon/common/validation/EmailFormatValidator.java
+++ src/main/java/com/kongtoon/common/validation/EmailFormatValidator.java
@@ -11,8 +11,27 @@ import java.util.regex.Pattern;
 @Component
 public class EmailFormatValidator implements ConstraintValidator<EmailValid, Email> {
 
+	private static final String INVALID_EMAIL_ADDRESS_MESSAGE = "잘못된 형식의 이메일입니다.";
+	private static final String ADDRESS_FIELD = "address";
+
 	@Override
 	public boolean isValid(Email email, ConstraintValidatorContext context) {
-		return Pattern.matches(RegexConst.EMAIL_VALID_REGEX, email.getAddress());
+		if (validateEmailAddress(email)) {
+			addConstraintViolation(context, INVALID_EMAIL_ADDRESS_MESSAGE, ADDRESS_FIELD);
+			return false;
+		}
+
+		return true;
+	}
+
+	private static boolean validateEmailAddress(Email email) {
+		return !Pattern.matches(RegexConst.EMAIL_VALID_REGEX, email.getAddress());
+	}
+
+	private void addConstraintViolation(ConstraintValidatorContext context, String errorMessage, String firstNode) {
+		context.disableDefaultConstraintViolation();
+		context.buildConstraintViolationWithTemplate(errorMessage)
+				.addPropertyNode(firstNode)
+				.addConstraintViolation();
 	}
 }

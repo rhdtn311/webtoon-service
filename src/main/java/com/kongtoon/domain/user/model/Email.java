88222f05b016a0ddf92d721b567471accc739cd4
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2023-08-20 14:30:34 +0900

    chore: `Email` 클래스에 `@ToString` 제거, `@AllArgsConstructor` 추가

--- src/main/java/com/kongtoon/domain/user/model/Email.java
+++ src/main/java/com/kongtoon/domain/user/model/Email.java
@@ -2,18 +2,18 @@ package com.kongtoon.domain.user.model;
 
 import com.kongtoon.common.validation.EmailValid;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import javax.validation.constraints.NotBlank;
 
-@ToString
 @EmailValid
 @Getter
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Embeddable
 public class Email {
@@ -22,8 +22,4 @@ public class Email {
     @NotBlank
     @Column(name = "email", unique = true, length = 320, nullable = false)
     private String address;
-
-    public Email(String address) {
-        this.address = address;
-    }
 }

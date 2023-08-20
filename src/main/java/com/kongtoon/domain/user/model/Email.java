120a17ed47eff143de67ee1eb6bd57f3743a8bac
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2023-08-21 04:21:20 +0900

    refactoring: 정규식을 검사할 때 불필요한 Pattern 객체 생성을 막고자 캐싱
    - `javax.validation.constraints.Pattern`을 사용하여 정규식 검증을 하면 `org.hibernate.validator.internal.constraintvalidators.bv.PatternValidator`가 사용되는데, 이는 내부적으로 `Pattern` 인스턴스를 재사용한다.

--- src/main/java/com/kongtoon/domain/user/model/Email.java
+++ src/main/java/com/kongtoon/domain/user/model/Email.java
@@ -1,6 +1,6 @@
 package com.kongtoon.domain.user.model;
 
-import com.kongtoon.common.validation.EmailValid;
+import com.kongtoon.common.constant.RegexConst;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -10,14 +10,17 @@ import org.hibernate.validator.constraints.Length;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
-@EmailValid
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Embeddable
 public class Email {
 
+    private static final String INVALID_EMAIL_MESSAGE = "잘못된 형식의 이메일입니다.";
+
+    @Pattern(regexp = RegexConst.EMAIL_VALID_REGEX, message = INVALID_EMAIL_MESSAGE)
     @Length(max = 320)
     @NotBlank
     @Column(name = "email", unique = true, length = 320, nullable = false)
